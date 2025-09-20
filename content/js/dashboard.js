33640308cf829ab11803b1bbdda6458b4a095f42
kkkrzychu <kkkrzychu@users.noreply.github.com>
2025-09-20 08:21:20 +0000

    deploy: 16ed3aac51519d0d360534b40b35da0a4db34cb5

--- content/js/dashboard.js
+++ content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 50, 0, 0.0, 66.85999999999999, 22, 493, 26.0, 171.8999999999999, 428.6499999999996, 493.0, 50.100200400801604, 1406.363508266533, 6.751784819639279], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["HTTP Request", 50, 0, 0.0, 66.85999999999999, 22, 493, 26.0, 171.8999999999999, 428.6499999999996, 493.0, 50.100200400801604, 1406.363508266533, 6.751784819639279], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 50, 0, 0.0, 39.52000000000002, 8, 408, 11.0, 80.79999999999994, 336.24999999999955, 408.0, 56.68934240362812, 1591.4846407312925, 7.639774659863946], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["HTTP Request", 50, 0, 0.0, 39.52000000000002, 8, 408, 11.0, 80.79999999999994, 336.24999999999955, 408.0, 56.68934240362812, 1591.4846407312925, 7.639774659863946], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
